832ec7c8a75d09b92da4e2c2dfec1a637d91e9bb
DeenoDev <deyanoklaasen@outlook.com>
2024-01-31 23:09:06 +0200

    Added conditional logic to list item to choose between plus or minus class for css styling.

--- src/components/TransactionList.vue
+++ src/components/TransactionList.vue
@@ -1,8 +1,8 @@
 <template>
    <h3>History</h3>
       <ul id="list" class="list">
-        <li v-for="transaction in transactions" v-bind:key="transaction.id">
-          {{transaction.text}} <span>-${{transaction.amount}}</span><button class="delete-btn">x</button>
+        <li v-for="transaction in transactions" v-bind:key="transaction.id" v-bind:class="transaction.amount < 0 ? 'minus' : 'plus'" >
+          {{transaction.text}} <span>${{transaction.amount}}</span><button class="delete-btn">x</button>
         </li>
         
       </ul>
