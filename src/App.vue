730e0b53428c46eb7024e04c977da63eb2e954c0
DeenoDev <deyanoklaasen@outlook.com>
2024-01-31 22:15:23 +0200

    IncomeExpenses component imported into App.vue file.

--- src/App.vue
+++ src/App.vue
@@ -9,11 +9,13 @@
 <script>
    import Header from './components/Header.vue';
    import Balance from './components/Balance.vue';
+   import IncomeExpenses from './components/IncomeExpenses.vue';
 
    export default {
     components: {
       Header,
       Balance,
+      IncomeExpenses,
     }
    }
 </script>
