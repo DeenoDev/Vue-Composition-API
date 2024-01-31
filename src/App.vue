5fa0ebfdc0d916c85fc97d2b6491a87c4c8ddfcf
DeenoDev <deyanoklaasen@outlook.com>
2024-01-31 22:26:19 +0200

    created and imported transaction list component.

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,7 @@
    <Header />
    <div class="container">
     <Balance />
+    <IncomeExpenses />
 
    </div>
 </template>
@@ -10,12 +11,14 @@
    import Header from './components/Header.vue';
    import Balance from './components/Balance.vue';
    import IncomeExpenses from './components/IncomeExpenses.vue';
+   import TransactionList from './components/TransactionList.vue';
 
    export default {
     components: {
       Header,
       Balance,
       IncomeExpenses,
+      TransactionList,
     }
    }
 </script>
--- src/components/IncomeExpenses.vue
+++ src/components/IncomeExpenses.vue
@@ -2,11 +2,11 @@
     <div class="inc-exp-container">
       <div>
         <h4>Income</h4>
-        <p id="money-plus" class="money plus">+$</p>
+        <p id="money-plus" class="money plus">+$0.00</p>
       </div>
       <div>
         <h4>Expense</h4>
-        <p id="money-minus" class="money minus">-$</p>
+        <p id="money-minus" class="money minus">-$0.00</p>
       </div>
     </div>
   </template>
