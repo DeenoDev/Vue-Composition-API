22f130b893241222843c1f2175c767a67e429f5b
DeenoDev <deyanoklaasen@outlook.com>
2024-02-01 21:43:17 +0200

    Used script setup method for composition API which is a lot shorter.

--- src/components/TransactionList.vue
+++ src/components/TransactionList.vue
@@ -9,15 +9,14 @@
   </template>
 
 
-<script>
-  export default {
-   setup(){
+<script setup>
+
+
     const transactions = [
           {id: 1, text: 'Flower', amount: -19.99},
           {id: 2, text: 'Salary', amount: 299.97},
           {id: 3, text: 'Book', amount: -10},
           {id: 4, text: 'Camera', amount: 150},
-        ]
-   }
-  }
+        ];
+ 
 </script>
