dcd5679770ff44f1b89fa78943f359fa38e37ba9
DeenoDev <deyanoklaasen@outlook.com>
2024-02-01 21:45:59 +0200

    converted App.vue script into setup composition API.

--- src/App.vue
+++ src/App.vue
@@ -9,20 +9,10 @@
    </div>
 </template>
 
-<script>
+<script setup>
    import Header from './components/Header.vue';
    import Balance from './components/Balance.vue';
    import IncomeExpenses from './components/IncomeExpenses.vue';
    import TransactionList from './components/TransactionList.vue';
    import AddTransaction from './components/AddTransaction.vue';
-
-   export default {
-    components: {
-      Header,
-      Balance,
-      IncomeExpenses,
-      TransactionList,
-      AddTransaction,
-    }
-   }
 </script>
