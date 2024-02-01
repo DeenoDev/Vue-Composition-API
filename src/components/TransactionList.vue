85a55cf52d1dc17fa8f7ba6fbd6fdc1697dc5bde
DeenoDev <deyanoklaasen@outlook.com>
2024-02-01 21:38:48 +0200

    converted options API setup into compositional API setup.

--- src/components/TransactionList.vue
+++ src/components/TransactionList.vue
@@ -11,15 +11,13 @@
 
 <script>
   export default {
-    data(){
-      return {
-        transactions: [
+   setup(){
+    const transactions = [
           {id: 1, text: 'Flower', amount: -19.99},
           {id: 2, text: 'Salary', amount: 299.97},
           {id: 3, text: 'Book', amount: -10},
           {id: 4, text: 'Camera', amount: 150},
         ]
-      }
-    }
+   }
   }
 </script>
