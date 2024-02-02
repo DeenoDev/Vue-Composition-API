b2323db2b17215374db1fdf6e10aae4d0fe02222
DeenoDev <deyanoklaasen@outlook.com>
2024-02-02 22:13:01 +0200

    Used Script setup to define income variable props in IncomeExpenses.vue app.

--- src/components/IncomeExpenses.vue
+++ src/components/IncomeExpenses.vue
@@ -10,3 +10,15 @@
       </div>
     </div>
   </template>
+
+  <script setup>
+     import {defineProps} from 'vue';
+
+     const props = defineProps({
+      income: {
+        type: Number,
+        require: true,
+      }
+     })
+     
+  </script>
