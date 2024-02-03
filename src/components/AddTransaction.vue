8cbab8d5b3c1b4d33ae16ee624ceaf91d03c4b81
DeenoDev <deyanoklaasen@outlook.com>
2024-02-03 23:29:39 +0200

    Added v-model directive to form input.

--- src/components/AddTransaction.vue
+++ src/components/AddTransaction.vue
@@ -3,14 +3,14 @@
       <form id="form" @submit.prevent="onSubmit">
         <div class="form-control">
           <label for="text">Text</label>
-          <input type="text" id="text" placeholder="Enter text..." />
+          <input type="text" v-model="text" id="text" placeholder="Enter text..." />
         </div>
         <div class="form-control">
           <label for="amount"
             >Amount <br />
             (negative - expense, positive - income)</label
           >
-          <input type="number" id="amount" placeholder="Enter amount..." />
+          <input type="text" id="amount" v-model="amount" placeholder="Enter amount..." />
         </div>
         <button class="btn">Add transaction</button>
       </form>
@@ -18,6 +18,6 @@
 
 <script setup>
   const onSubmit = () => {
-    console.log(submit);
-  }
+    console.log('submit');
+  };
 </script>
