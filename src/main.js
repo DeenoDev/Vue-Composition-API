a534d62e3788b071b62fc5e89ea288cfbba4d15d
DeenoDev <deyanoklaasen@outlook.com>
2024-02-03 23:37:40 +0200

    imported vue-toastificartion in main.js file.

--- src/main.js
+++ src/main.js
@@ -1,6 +1,9 @@
-import './assets/style.css'
+import { createApp } from 'vue';
 
-import { createApp } from 'vue'
-import App from './App.vue'
 
-createApp(App).mount('#app')
+import Toast from 'vue-toastification';
+import 'vue-toastification/dist/index.css';
+import './assets/style.css';
+import App from './App.vue';
+
+createApp(App).mount('#app');
