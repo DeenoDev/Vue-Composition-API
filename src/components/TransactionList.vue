af9ab698538354c67d0797a89105364e8e1b8034
DeenoDev <deyanoklaasen@outlook.com>
2024-02-05 23:18:34 +0200

    deleteTransaction function attached to click event listener on button in TransactionList.vue.

--- src/components/TransactionList.vue
+++ src/components/TransactionList.vue
@@ -2,7 +2,8 @@
    <h3>History</h3>
       <ul id="list" class="list">
         <li v-for="transaction in transactions" v-bind:key="transaction.id" v-bind:class="transaction.amount < 0 ? 'minus' : 'plus'" >
-          {{transaction.text}} <span>${{transaction.amount}}</span><button class="delete-btn">x</button>
+          {{transaction.text}} <span>${{transaction.amount}}</span>
+          <button @click="deleteTransaction(transaction.id)" class="delete-btn">x</button>
         </li>
       </ul>
   </template>
